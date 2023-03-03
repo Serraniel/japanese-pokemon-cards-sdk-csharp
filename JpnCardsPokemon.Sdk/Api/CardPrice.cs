054e72cc3361bd74fc405270c99a43b85abc6102
Serraniel <mail@serraniel.dev>
2023-03-03 15:23:12 +0100

    Extends the card price class by additional information provided by the API
    In particular Seller, condition and version

--- JpnCardsPokemon.Sdk/Api/CardPrice.cs
+++ JpnCardsPokemon.Sdk/Api/CardPrice.cs
@@ -5,13 +5,17 @@ namespace JpnCardsPokemon.Sdk.Api;
 
 public class CardPrice
 {
-    public string Condition { get; set; }
+    public string? Version { get; set; }
+
+    public string? Condition { get; set; }
 
     [JsonPropertyName("priceAmount")] public decimal Price { get; set; }
 
-    [JsonPropertyName("priceCurrency")] public string Currency { get; set; }
+    [JsonPropertyName("priceCurrency")] public string? Currency { get; set; }
 
     [JsonPropertyName("dateUpdated")] public DateTime? UpdatedDate { get; set; }
 
     public string? ListingUrl { get; set; }
+
+    public string? Seller { get; set; }
 }

6fd5987d644f0b7058cab1fa75c6ca035dd86fed
Serraniel <mail@serraniel.dev>
2023-03-03 15:24:06 +0100

    Adds basic tests for card prices

--- JpnCardsPokemon.Tests/CardTests.cs
+++ JpnCardsPokemon.Tests/CardTests.cs
@@ -11,4 +11,18 @@ public class CardTests : ApiTestClass
         var cards = await Client.FetchCardsAsync(new CardQueryFilterBuilder { Name = "morpeko" });
         Assert.IsNotEmpty(cards);
     }
+
+    [Test]
+    public async Task TestGetPrices()
+    {
+        // Radiant charizard from Go may have prices in different conditions
+        var card = (await Client.FetchCardsAsync(new CardQueryFilterBuilder { Uuid = 48495360 })).FirstOrDefault();
+        Assert.IsNotNull(card);
+        Assert.IsNotEmpty(card.Prices);
+
+        // Zubat, common from Dark Phantasma may have prices for different versions (regular + reverse holo)
+        card = (await Client.FetchCardsAsync(new CardQueryFilterBuilder { Uuid = 95800678 })).FirstOrDefault();
+        Assert.IsNotNull(card);
+        Assert.IsNotEmpty(card.Prices);
+    }
 }
